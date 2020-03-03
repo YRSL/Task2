26e2e25cc401b16879c3b1dec1b8c426c4e8039a
Ruslan <yakovchik.ruslan@gmail.com>
2020-03-03 11:02:13 +0300

    Added functions shorten_line, parse_digits, find_values.

--- task_2.py
+++ task_2.py
@@ -3,49 +3,70 @@ from functools import total_ordering
 
 class Version:
 
-    def __init__(self, version: str):
-        self.ver2 = str()
-        self.version = version
-
     def __init__(self, version):
         self.version = version
 
     def __str__(self):
         return self.version
 
-    def convert_into_ord(self, ver2):
-        el1 = [i for i in self.version.split('.')]
-        el2 = [i for i in self.ver2.split('.')]
-        po = dict(zip(el1, el2))
-        po = {k: v for k, v in po.items() if k != v}
-        if len(po) != 0:
-            so = list(po.items())[0]
-            result_item1 = so[0]
-            result_item2 = so[1]
-            res_gen = [ord(i) for i in result_item1]
-            res_gen1 = [ord(i) for i in result_item2]
-            if len(res_gen) > len(res_gen1):
-                res_gen = res_gen[:len(res_gen1) + 1]
-                res_gen1.append(0)
-            elif len(res_gen) < len(res_gen1):
-                res_gen1 = res_gen1[:len(res_gen) + 1]
-                res_gen.append(0)
-            el1x = [i for i in res_gen]
-            el2x = [i for i in res_gen1]
-            zo = dict(zip(el1x, el2x))
-            zo = {k: v for k, v in zo.items() if k != v}
-            so = list(zo.items())[0]
-            return so[0], so[1]
+    @staticmethod
+    def shorten_line(object1, object2):
+        found_line = ()
+        elements_dict = list(zip(object1, object2))
+        for item in elements_dict:
+            if len(set(item)) > 1:
+                found_line = item
+                break
+        if found_line is ():
+            if object1 == object2:
+                return '1', '1'
+            elif len(object1) > len(object2):
+                return '0', '1'
+            elif len(object1) < len(object2):
+                return '1', '0'
+        else:
+            return found_line
+
+    @staticmethod
+    def parse_digits(line):
+        digits_list = []
+        for item in line:
+            if not item.isdigit():
+                break
+            else:
+                digits_list.append(item)
+        parsed_digit = ''.join(digits_list)
+        return parsed_digit
+
+    def find_values(self, other):
+        version1 = str(self).replace('-', '')
+        version2 = str(other).replace('-', '')
+        elements_list_ver1 = [i for i in version1.split('.')]
+        elements_list_ver2 = [i for i in version2.split('.')]
+        found_line = self.shorten_line(elements_list_ver1, elements_list_ver2)
+        if found_line[0].isdigit() and found_line[1].isdigit():
+            return found_line[0], found_line[1]
+        else:
+            digits_new_version1 = self.parse_digits(found_line[0])
+            digits_new_version2 = self.parse_digits(found_line[1])
+            if digits_new_version1 != digits_new_version2:
+                return digits_new_version1, digits_new_version2
+            else:
+                start = len(digits_new_version1)
+                result_found_line = self.shorten_line(found_line[0][start:], found_line[1][start:])
+                if result_found_line[0].isdigit() and result_found_line[1].isdigit():
+                    return int(result_found_line[0]), int(result_found_line[1])
+                else:
+                    return ord(result_found_line[0]), ord(result_found_line[1])
 
     @total_ordering
     def __eq__(self, version_2):
-        so = self.convert_into_ord(version_2)
-        if so is None:
-            return int(so[0]) == int(so[1])
+        x = self.find_values(version_2)
+        return x[0] == x[1]
 
     def __lt__(self, version_2):
-        so = self.convert_into_ord(version_2)
-        return int(so[0]) < int(so[1])
+        x = self.find_values(version_2)
+        return x[0] < x[1]
 
 
 def main():
